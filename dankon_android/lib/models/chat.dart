1d6877823c8bef82092286b62e6300e0836d14cb
Stanisław Dera <stanislawdera@outlook.com>
2022-01-05 00:29:33 +0100

    Remove .utc from dankstreak system

--- dankon_android/lib/models/chat.dart
+++ dankon_android/lib/models/chat.dart
@@ -66,7 +66,7 @@ class Chat {
   }
 
   bool startNewDankstreak() {
-    DateTime from = DateTime.utc(lastDankstreakTime.year,
+    DateTime from = DateTime(lastDankstreakTime.year,
         lastDankstreakTime.month, lastDankstreakTime.day);
     int difference = DateTime.now().difference(from).inDays;
 
@@ -78,7 +78,7 @@ class Chat {
   }
 
   int countDays() {
-    DateTime from = DateTime.utc(
+    DateTime from = DateTime(
         dankstreakFrom.year, dankstreakFrom.month, dankstreakFrom.day);
     return lastDankstreakTime.difference(from).inDays;
   }
