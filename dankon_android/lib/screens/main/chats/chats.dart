9bd58a7948b4cc06bc47072168182216a02f3a4f
Stanisław Dera <stanislawdera@outlook.com>
2022-01-05 15:40:52 +0100

    Add chat tab

--- dankon_android/lib/screens/main/chats/chats.dart
+++ dankon_android/lib/screens/main/chats/chats.dart
@@ -1,18 +1,32 @@
+import 'package:dankon/models/chat.dart';
+import 'package:dankon/widgets/chat_tile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatsPage extends StatelessWidget {
   const ChatsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
-        Text("Comming soon", style: TextStyle(fontSize: 28),),
-        SizedBox(height: 5,),
-        Text("We are going to introduce chat in future.")
-      ],
+    String myUid = context.read<User?>()!.uid;
+    List<Chat>? chats = context.watch<List<Chat>?>();
+
+    return chats == null ? const Center(child: CircularProgressIndicator(),) : ListView(
+      physics: const BouncingScrollPhysics(),
+      children: chats.map((Chat chat) {
+
+        String title = chat.getChatName(myUid);
+        String image = chat.getChatImageUrl(myUid);
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: ChatTile(
+              title: title,
+              imageUrl: image,
+          ),
+        );
+      }).toList(),
     );
   }
 }
--- dankon_android/lib/widgets/chat_tile.dart
+++ dankon_android/lib/widgets/chat_tile.dart
@@ -3,18 +3,20 @@ import 'package:dankon/widgets/cached_avatar.dart';
 import 'package:flutter/material.dart';
 
 class ChatTile extends StatelessWidget {
-  const ChatTile({Key? key, required this.subtitle, required this.title, required this.imageUrl, this.trailing}) : super(key: key);
-  final String subtitle;
+  const ChatTile({Key? key, this.subtitle, required this.title, required this.imageUrl, this.trailing, this.onPressed}) : super(key: key);
+  final String? subtitle;
   final String title;
   final String imageUrl;
   final Widget? trailing;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
         title: Text(title),
-        subtitle: Text(subtitle),
-        leading: CachedAvatar(url: getAccessUrlIfFacebook(imageUrl),),
+        onTap: onPressed,
+        subtitle: subtitle != null ? Text(subtitle!) : null,
+        leading: CachedAvatar(url: getAccessUrlIfFacebook(imageUrl), radius: 22,),
         trailing: trailing
     );
   }
