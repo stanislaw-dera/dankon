45736a24f44a6be325771bbf980cf3473b27a4e5
Stanisław Dera <stanislawdera@outlook.com>
2022-01-29 19:42:30 +0100

    Add on cleanUpAfterMessageDeletion cloud function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -49,7 +49,7 @@ exports.sendDankNotification = functions.firestore
       },
     };
 
-    // sent notification
+    // send notification
     const messaging = admin.messaging();
     await messaging.sendToDevice(reciverTokens, payload);
   });
@@ -112,7 +112,7 @@ exports.sendChatNotification = functions.firestore
       tokens: reciverTokens,
     };
 
-    // sent notification
+    // send notification
     const messaging = admin.messaging();
     await messaging.sendMulticast(message);
 
@@ -122,4 +122,83 @@ exports.sendChatNotification = functions.firestore
       lastMessageAuthor: senderName.split(" ")[0],
       lastMessageContent: messageData.content
     })
+  });
+
+  exports.cleanUpAfterMessageDeletion = functions.firestore
+  .document("chats/{chatId}/messages/{messageId}")
+  .onDelete(async (snap, context) => {
+    const db = admin.firestore();
+
+    const messageData = snap.data();
+
+    const chatId = context.params.chatId;
+    const chatDocRef = db.collection("chats").doc(chatId);
+
+    const chatDoc = await chatDocRef.get();
+    const chatData = chatDoc.data();
+
+    // Get previous message
+    let previousMessage = {};
+    const previousMessagesSnapshot = await db.collection(`chats/${chatId}/messages`).orderBy("time", "desc").limit(1).get();
+    previousMessagesSnapshot.forEach(doc => {
+      previousMessage = doc.data();
+    });
+
+    let senderName = "";
+    chatData.participantsData.forEach((participant) => {
+      if (previousMessage.author == participant.uid) {
+        senderName = participant.name;
+      }
+    });
+
+    // Update last message fields in chat document
+    await chatDocRef.update({
+      lastMessageTime: previousMessage.time,
+      lastMessageAuthor: senderName.split(" ")[0],
+      lastMessageContent: previousMessage.content
+    })
+
+    // get notification reciver uid
+    let notificationReciverUid = null;
+    chatData.participantsData.forEach((participant) => {
+      if (messageData.author != participant.uid) {
+        notificationReciverUid = participant.uid;
+      }
+    });
+
+    functions.logger.log(`Reciver uid is ${notificationReciverUid}`);
+
+    // get recivger's tokens
+    const reciverRef = db.collection("users").doc(notificationReciverUid);
+    const reciverDoc = await reciverRef.get();
+    const reciverDocData = reciverDoc.data();
+    const reciverTokens = reciverDocData.notificationsTokens;
+
+    functions.logger.log("Got reciver tokens", { tokens: reciverTokens });
+
+    // check if there are any tokens
+    if (reciverTokens.length == 0) {
+      return;
+    }
+
+    // update notification
+    const message = {
+      data: {
+        type: "CHAT/NEW_MESSAGE",
+        id: chatId,
+        senderName: "Somebody",
+      },
+      notification: {
+        title: "Somebody",
+        body: "Deleted messages"
+      },
+      android: {
+        notification: {
+          tag: chatId
+        }
+      },
+      tokens: reciverTokens,
+    };
+    const messaging = admin.messaging();
+    await messaging.sendMulticast(message);
   });
