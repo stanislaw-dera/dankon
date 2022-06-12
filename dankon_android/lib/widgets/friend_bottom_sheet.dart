0b1f1e7dccd65e87479257bc2f646e40c1b1490a
Stanisław Dera <stanislaw.dera@outlook.com>
2022-06-12 21:40:17 +0200

    Add message input in friend bottom sheet

--- dankon_android/lib/widgets/friend_bottom_sheet.dart
+++ dankon_android/lib/widgets/friend_bottom_sheet.dart
@@ -1,5 +1,10 @@
+import 'package:dankon/constants/chat_themes.dart';
 import 'package:dankon/models/chat.dart';
+import 'package:dankon/models/chat_theme.dart';
+import 'package:dankon/services/database.dart';
+import 'package:dankon/services/read_receipt.dart';
 import 'package:dankon/widgets/cached_avatar.dart';
+import 'package:dankon/widgets/message_input.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,31 +12,32 @@ import 'package:provider/provider.dart';
 void showFriendBottomSheet(Chat chat, BuildContext context) {
   showModalBottomSheet(
       context: context,
+      isScrollControlled: true,
       builder: (BuildContext context) {
         return FriendBottomSheet(chat: chat);
       });
 }
 
 Widget buildOverlappingAvatars(Chat chat, User? me) => SizedBox(
-    width: 36 + 36 + 50,
-    child: Stack(
-      children: <Widget>[
-        Positioned(
-          child: CachedAvatar(
-            url: me!.photoURL.toString(),
-            radius: 36,
+      width: 36 + 36 + 50,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            child: CachedAvatar(
+              url: me!.photoURL.toString(),
+              radius: 36,
+            ),
           ),
-        ),
-        Positioned(
-          left: 50,
-          child: CachedAvatar(
-            url: chat.getChatImageUrl(me.uid),
-            radius: 36,
+          Positioned(
+            left: 50,
+            child: CachedAvatar(
+              url: chat.getChatImageUrl(me.uid),
+              radius: 36,
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
 
 class FriendBottomSheet extends StatelessWidget {
   const FriendBottomSheet({Key? key, required this.chat}) : super(key: key);
@@ -44,6 +50,7 @@ class FriendBottomSheet extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           buildOverlappingAvatars(chat, me),
           const SizedBox(
@@ -52,6 +59,28 @@ class FriendBottomSheet extends StatelessWidget {
           Text(
             "You and ${chat.getChatName(me!.uid).split(" ")[0]}",
             style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+              child: Provider<ChatTheme>.value(
+                  value: getChatThemeById("default"),
+                  child: MessageInput(
+                      chatId: chat.id,
+                      databaseService: DatabaseService(uid: me.uid),
+                      onMessageSent: () {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Message sent!")));
+                        ReadReceiptService(chat: chat, uid: me.uid).updateReadReceipt();
+                        Navigator.of(context).pop();
+                      },
+                  ))),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: const SizedBox(
+              height: 20,
+            ),
           )
         ],
       ),
