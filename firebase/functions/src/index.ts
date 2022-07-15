17f68092e73154dc477fb779a4c2c6b1b2664577
Stanisław Dera <stanislaw.dera@outlook.com>
2022-07-15 09:39:04 +0200

    Add firebase-admin initialization

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,3 +1,7 @@
+import * as admin from "firebase-admin"
+
+admin.initializeApp()
+
 export { finishTicTacToe } from "./finishTicTacToe"
 export { handleMessageDeletion } from "./handleMessageDeletion"
 export { sendChatNotification } from "./sendChatNotification"
